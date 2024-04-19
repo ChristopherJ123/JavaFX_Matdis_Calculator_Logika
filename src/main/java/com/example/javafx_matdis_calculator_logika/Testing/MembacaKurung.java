c17b6be453c3d48c7a84cf6f56c8a14df24e725e
ChristopherJ123 <christopherz2002@hotmail.com>
2024-04-19 22:17:28 +0700

    tambahan hukum 1, 2, 3, 10, 11, 12, 14 🙃🔥 sulit

--- src/main/java/com/example/javafx_matdis_calculator_logika/Testing/MembacaKurung.java
+++ src/main/java/com/example/javafx_matdis_calculator_logika/Testing/MembacaKurung.java
@@ -12,15 +12,50 @@ public class MembacaKurung {
         return item.charAt(0) == 'n';
     }
 
-    static boolean isOperator(String operator) {
-        return operator.matches("[aoib^v]");
-    }
-
     static String switchPositiveNegative(String item) {
         if (isNegative(item)) return item.substring(1);
         else return "n" + item;
     }
 
+    // 1. Hukum Identitas
+    static boolean hkIdentitas(String[] param) {
+        if ((param[1].equals("o") && param[2].equals("f")) || (param[1].equals("a") && param[2].equals("t"))) {
+            param[1] = "";
+            param[2] = "";
+            return true;
+        }
+        return false;
+    }
+
+    // 2. Hukum Dominasi
+    static boolean hkDominasi(String[] param) {
+        if ((param[1].equals("o") && param[2].equals("t"))) {
+            param[0] = "t";
+            param[1] = "";
+            param[2] = "";
+            return true;
+        } else if ((param[1].equals("a") && param[2].equals("f"))) {
+            param[0] = "f";
+            param[1] = "";
+            param[2] = "";
+            return true;
+        }
+        return false;
+    }
+
+    // 3. Hukum Idempoten
+    static boolean hkIdempoten(String[] param) {
+        if (param[1].equals("a") || param[1].equals("o")) {
+            if (param[0].equals(param[2])) {
+                param[1] = "";
+                param[2] = "";
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 9. Hukum Implikasi
     static boolean hkImplikasi(String[] param) {
         if (param[1].equals("i")) {
             param[0] = switchPositiveNegative(param[0]);
@@ -30,9 +65,62 @@ public class MembacaKurung {
         return false;
     }
 
+    // 10. Hukum Kontraposisi
+    static boolean hkKontraposisi(String[] param) {
+        if (param[1].equals("i")) {
+            String temp = param[0];
+            param[0] = switchPositiveNegative(param[2]);
+            param[2] = switchPositiveNegative(temp);
+            return true;
+        }
+        return false;
+    }
+
+    // 11. Hukum Tautologi
+    static boolean hkTautologi(String[] param) {
+        if (param[1].equals("o") && param[2].equals(switchPositiveNegative(param[0]))) {
+            param[0] = "t";
+            param[1] = "";
+            param[2] = "";
+            return true;
+        }
+        return false;
+    }
+
+    // 12. Hukum Kontradiksi
+    static boolean hkKontradiksi(String[] param) {
+        if (param[1].equals("a") && param[2].equals(switchPositiveNegative(param[0]))) {
+            param[0] = "f";
+            param[1] = "";
+            param[2] = "";
+            return true;
+        }
+        return false;
+    }
+
+    // 14. Hukum Bi-implikasi
+    static boolean hkBiimplikasi(String[] param) {
+        if (param[1].equals("b")) {
+            String temp = param[0];
+            param[0] = "(" + param[0] + " i " + param[2] + ")";
+            param[1] = "a";
+            param[2] = "(" + param[2] + " i " + temp + ")";
+            return true;
+        }
+        return false;
+    }
+
+
     static String simplify(String item) {
         String[] items = item.split(" ");
-        hkImplikasi(items);
+        hkIdentitas(items); //1
+        hkDominasi(items); //2
+        hkIdempoten(items); //3
+        hkImplikasi(items); //9
+        hkKontraposisi(items); //10
+        hkTautologi(items); //11
+        hkKontradiksi(items); //12
+        hkBiimplikasi(items); //14
         return Arrays.toString(items).replaceAll("[\\[, \\]]", "");
     }
 
@@ -123,6 +211,43 @@ public class MembacaKurung {
         System.out.println("FINAL RESULT: " + input);
     }
 }
+
+/*
+Testing Prompts
+1. Hukum Identitas
+(P o f) == P
+((P o f) a (P a t)) == P
+
+2. Hukum Dominasi
+(P o t) == t
+(P a f) == f
+((P o t) a (P a f)) == t a f
+
+3. Hukum Idempoten
+(P o P) == P
+(P a P) == P
+((P o P) a (P a P)) == P
+((P o P) a (P a P) o (P o P)) == P
+
+9. Hukum Implikasi
+(P i nQ) == (nPonQ) == nP o NQ
+((P i nQ) i (nP i Q)) == (n(nPonQ)o(PoQ)) == n((nP o nQ) o (P o Q))
+(((Q i P) i (P i (P i Q))) a (P i nQ) o (nP i Q) i (nP i nQ)) == (((((QonP)onP)on(PonQ))a(nQonP))o((nQoP)on(QoP)))
+
+10. Hukum Kontraposisi
+()
+
+11. Hukum Tautologi
+(P o nP) == t
+((P o nP) o (P o nP)) == (t o t) == t (hukum idempoten)
+
+12. Hukum Kontradiksi
+(P a nP) == f
+
+14. Hukum Bi-implikasi
+(P b Q) == ((PiQ)a(QiP)) == (P i Q) a (Q i P)
+ */
+
 // (P i nQ)
 // (P i nQ) i (nP a Q)
 // (P a (P i nQ)) i (nP a Q)
