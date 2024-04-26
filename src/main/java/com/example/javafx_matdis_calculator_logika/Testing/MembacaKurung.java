22e727d08e2f9df57830e82c954a0ae49bbceaf4
ChristopherJ123 <christopherz2002@hotmail.com>
2024-04-26 21:54:57 +0700

    new things (wip)

--- src/main/java/com/example/javafx_matdis_calculator_logika/Testing/MembacaKurung.java
+++ src/main/java/com/example/javafx_matdis_calculator_logika/Testing/MembacaKurung.java
@@ -111,29 +111,97 @@ public class MembacaKurung {
     }
 
 
-    static String simplify(String item) {
+    static String[] simplify(String item) {
         String[] items = item.split(" ");
-        hkIdentitas(items); //1
-        hkDominasi(items); //2
-        hkIdempoten(items); //3
-        hkImplikasi(items); //9
-        hkKontraposisi(items); //10
-        hkTautologi(items); //11
-        hkKontradiksi(items); //12
-        hkBiimplikasi(items); //14
-        return Arrays.toString(items).replaceAll("[\\[, \\]]", "");
+        String hukum = "";
+        if (hkIdentitas(items)) hukum += "Hk. Identitas"; //1
+        if (hkDominasi(items)) hukum += "Hk. Dominasi"; //2
+        if (hkIdempoten(items)) hukum += "Hk. Idempoten"; //3
+        if (hkImplikasi(items)) hukum += "Hk. Implikasi"; //9
+        if (hkKontraposisi(items)) hukum += "Hk. Kontraposisi"; //10
+        if (hkTautologi(items)) hukum += "Hk. Tautologi"; //11
+        if (hkKontradiksi(items)) hukum += "Hk. Dominasi"; //12
+        if (hkBiimplikasi(items)) hukum += "Hk. Bi-implikasi"; //14
+        if (hukum.equals("")) {
+            hukum = "None";
+        }
+        return new String[]{Arrays.toString(items).replaceAll("[\\[, \\]]", ""), hukum};
+    }
+
+    static boolean loopPenyederhanaanKurung() {
+        boolean masihLooping = false;
+        // Cek hukum dobel negasi
+        if (input.contains("n(n(P))") || input.contains("n(n(Q))")) {
+            String hukum = "Hk. Dobel Negasi";
+            input = input.replace("n(n(P))" , "P");
+            input = input.replace("n(n(Q))" , "Q");
+            System.out.println("Hukum: " + hukum);
+            System.out.println(input);
+            masihLooping = true;
+        }
+
+        // Sederhanain kurung
+        if (input.contains("(P)") || input.contains("(Q)")) {
+            if (input.length() == 3) return false;
+            for (int i = 2; i < input.length(); i++) {
+                if (input.substring(i-2, i+1).equals("(P)")) {
+                    try {
+                        if (input.charAt(i-3) != 'n') {
+                            input = input.substring(0, i-2).concat(String.valueOf(input.charAt(i-1)).concat(input.substring(i+1)));
+                            masihLooping = true;
+                            System.out.println("Terjadi penyederhanaan: " + input);
+                        }
+                    } catch (StringIndexOutOfBoundsException e) {
+                        input = input.substring(0, i-2).concat(String.valueOf(input.charAt(i-1)).concat(input.substring(i)));
+                        masihLooping = true;
+                        System.out.println("Terjadi penyederhanaan: " + input);
+                    }
+                }
+            }
+        }
+
+        // Cek hukum de morgan
+        if (input.contains("n(P o Q)") || input.contains("n(n(P) o Q)") || input.contains("n(P o n(Q))") || input.contains("n(n(P) o (Q))") ||
+                input.contains("n(Q o P)") || input.contains("n(n(Q) o P)") || input.contains("n(Q o n(P))") || input.contains("n(n(Q) o n(P))") ||
+                input.contains("n(P a Q)") || input.contains("n(n(P) a Q)") || input.contains("n(P a n(Q))") || input.contains("n(n(P) a n(Q))") ||
+                input.contains("n(Q a P)") || input.contains("n(n(Q) a P)") || input.contains("n(Q a n(P))") || input.contains("n(n(Q) a n(P))")
+        ) {
+            String hukum = "Hk. De Morgan";
+            input = input.replace("n(P o Q)", "n(P) a n(Q)");
+            input = input.replace("n(n(P) o Q)", "P a n(Q)");
+            input = input.replace("n(P o n(Q))", "n(P) a Q");
+            input = input.replace("n(n(P) o n(Q))", "P a Q");
+            input = input.replace("n(Q o P)", "n(Q) a n(P)");
+            input = input.replace("n(n(Q) o P)", "Q a n(P)");
+            input = input.replace("n(Q o n(P))", "n(Q) a P");
+            input = input.replace("n(n(Q) o n(P))", "Q a P");
+
+            input = input.replace("n(P a Q)", "n(P) o n(Q)");
+            input = input.replace("n(n(P) a Q)", "P o n(Q)");
+            input = input.replace("n(P a n(Q))", "n(P) o Q");
+            input = input.replace("n(n(P) a n(Q))", "P o Q");
+            input = input.replace("n(Q a P)", "n(Q) o n(P)");
+            input = input.replace("n(n(Q) a P)", "Q o n(P)");
+            input = input.replace("n(Q a n(P))", "n(Q) o P");
+            input = input.replace("n(n(Q) a n(P))", "Q o P");
+            System.out.println("Hukum: " + hukum);
+            masihLooping = true;
+        }
+        return masihLooping;
     }
 
     static boolean loopAndIterate() {
         boolean running = true;
         int index = 0;
-        while (running) {
+        while (running) { // Running samapai tidak ada spasi
             System.out.println("INDEX-OF: " + index);
             running = false;
             listInput = new ArrayList<>();
             listOutput = new ArrayList<>();
             HashMap<Integer, Integer> brackets = new HashMap<>();
             int bracketIndex = 0;
+
+            while (loopPenyederhanaanKurung());
 
             // Iterate through input chars
             for (int i = 0; i < input.length(); i++) {
@@ -152,27 +220,33 @@ public class MembacaKurung {
             // Duplicate listInput to listOutput
             listOutput.addAll(listInput);
 
+
+
             // Iterate through list
             for (int i = 0; i < listInput.size(); i++) {
+
                 int spaceCount = 0;
                 String item = listInput.get(i);
+
                 for (int j = 0; j < item.length(); j++) {
                     if (item.charAt(j) == ' ') spaceCount++;
                 }
+
                 if (spaceCount == 2) {
-                    listOutput.set(i, simplify(listInput.get(i)));
-                    System.out.println(listOutput.get(i));
+                    String[] simplified = simplify(listInput.get(i));
+                    listOutput.set(i, simplified[0]);
+                    System.out.print("Hukum: " + simplified[1] + "(" + listInput.get(i) + ")");
+                    System.out.println(" menjadi: " + listOutput.get(i));
                     running = true;
-                }
-                else {
-                    System.out.println("++++++++++");
+                } else {
+//                    System.out.println("++++++++++");
                     System.out.println("----------");
                     for (int j = i-1; j >= 0; j--) {
                         listOutput.set(i, listOutput.get(i).replace(listInput.get(j), listOutput.get(j)));
-                        System.out.println("CHECKED WITH: " + listInput.get(j) + " AFTER REPLACED: " + listOutput.get(i));
+                        System.out.println("REPLACE: " + listInput.get(j) + " WITH: " + listOutput.get(j) + " MENJADI: " + listOutput.get(i));
                     }
                     System.out.println("----------");
-                    System.out.println("++++++++++");
+//                    System.out.println("++++++++++");
                 }
             }
             input = "(" + listOutput.get(listOutput.size()-1) + ")";
@@ -186,6 +260,9 @@ public class MembacaKurung {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine();
+
+
+
         while (!loopAndIterate()) {
             input = "(".concat(input);
             int spaceCount = 0;
@@ -255,3 +332,9 @@ Testing Prompts
 // ((P i Q) a (P i nQ) o (nP i nQ) i (nP i nQ))
 // (((Q i P) i (P i (P i Q))) a (P i nQ) o (nP i Q) i (nP i nQ))
 // (((Q i P) i (P i (P i Q))) a (P i nQ) o (nP i Q))
+
+/*
+(n(n(P)))
+(n(P o Q))
+(n(n(n(n(P)))))
+ */
